fced0963625eeba65af156478f7702a1e4f46fdf
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2023-10-27 13:21:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,18 +8,50 @@ const BaseItems = lazy(() => import("pages/BaseItems"));
 const CustomItems = lazy(() => import("pages/CustomItems"));
 
 export const App = () => {
-
-    return (
-      <>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/base-items" element={<BaseItems />} />
-            <Route path="/custom-items" element={<CustomItems />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </>
-    )
+const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      return(parsedContacts);
     };
+});
+const [filter, setFilter] = useState('');
+
+useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+}, [contacts])
+
+const createContact = (values) => {
+  const targetContact = contacts
+    .map((cont) => cont.name.toLowerCase())
+    .includes(values.name.toLowerCase());
+
+    if (targetContact) {
+      alert(`${values.name} is already in contacts`);
+    } else {
+      values.id = nanoid();
+      setContacts(prevState => [...prevState, values]);
+    };  
+};
+
+const changeFilter = searchValue => {
+  setFilter(searchValue.target.value);
+}
+
+const handleDelete = (contactId) => {
+  setContacts(prevState => prevState.filter(contact => contact.id !== contactId));
+};
+
+const actualContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+
+  return (
+    <>
+      <GlobalStyle />
+      <AddContact create={createContact} />
+      <div>
+        <Filter onFilter={changeFilter} initValue={filter}/>
+        <ContactList actual={actualContacts} onDelete={handleDelete}/>
+      </div>
+    </>
+  );
+};
